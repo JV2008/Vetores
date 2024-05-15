f88a13f4cb0d0add4385f77e57d42963032eb07f
JV2008 <joaovictor25032@gmail.com>
2024-05-15 12:13:33 -0300

    primeiro commit

---
+++ NomesNoVetor.java
@@ -0,0 +1,30 @@
+package vetores;
+
+import java.util.Scanner;
+
+public class NomesNoVetor {
+
+	public static void main(String[] args) {
+			
+			String a[] = new String [5];
+			
+			
+			Scanner ler = new Scanner (System.in);
+			
+			for (int i=0; i<5; i++){
+				
+				System.out.println("Informe um nome " + i+ ". . : ");
+				a[i] = ler.nextLine();
+			}
+			for (int i =0; i<5;i++) {
+				System.out.println(a [i]);
+			}
+	ler.close();
+		
+
+	
+
+
+	}
+
+}
